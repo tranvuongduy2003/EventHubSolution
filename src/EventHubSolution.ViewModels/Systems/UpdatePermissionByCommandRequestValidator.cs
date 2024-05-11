bfad5ace514073a4db0e4c33331d17a0b54a6c08
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2024-05-12 04:07:46 +0700

    Permission Payload Validator

---
+++ src/EventHubSolution.ViewModels/Systems/UpdatePermissionByCommandRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace EventHubSolution.ViewModels.Systems
+{
+    public class UpdatePermissionByCommandRequestValidator : AbstractValidator<UpdatePermissionByCommandRequest>
+    {
+        public UpdatePermissionByCommandRequestValidator()
+        {
+            RuleFor(x => x.CommandId).NotEmpty().WithMessage("CommandId is required");
+            RuleFor(x => x.FunctionId).NotEmpty().WithMessage("FunctionId is required");
+            RuleFor(x => x.Value)
+                .NotNull()
+                .WithMessage("Value is required");
+        }
+    }
+}
---
+++ src/EventHubSolution.ViewModels/Systems/UpdatePermissionByRoleRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace EventHubSolution.ViewModels.Systems
+{
+    public class UpdatePermissionByRoleRequestValidator : AbstractValidator<UpdatePermissionByRoleRequest>
+    {
+        public UpdatePermissionByRoleRequestValidator()
+        {
+            RuleFor(x => x.RoleId).NotEmpty().WithMessage("RoleId is required");
+            RuleFor(x => x.FunctionId).NotEmpty().WithMessage("FunctionId is required");
+            RuleFor(x => x.Value)
+                .NotNull()
+                .WithMessage("Value is required");
+        }
+    }
+}
